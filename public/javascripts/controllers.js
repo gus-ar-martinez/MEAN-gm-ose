f10d5bc5b6d0fd3c057e869d4d5b80be93537e92
gus-ar-martinez <gus.ar.martinez@gmail.com>
2017-02-13 00:11:03 -0300

    Rollback

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -6,8 +6,6 @@ function PollListCtrl($scope, Poll) {
 // Controller for an individual poll
 function PollItemCtrl($scope, $routeParams, socket, Poll) {	
 	$scope.poll = Poll.get({pollId: $routeParams.pollId});
-	console.log('pregunta');
-console.log(poll.question);
 
 	socket.on('myvote', function(data) {
 		console.dir(data);
